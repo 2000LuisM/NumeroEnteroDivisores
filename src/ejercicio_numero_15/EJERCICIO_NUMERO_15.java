a7a1dae00f014ece123960e3c45a6ed6e4a7c05f
2000LuisM <luis.gonzales@unas.edu.pe>
2022-08-23 16:49:50 -0500

    divisores

---
+++ src/ejercicio_numero_15/EJERCICIO_NUMERO_15.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_numero_15;
+
+/**
+ *
+ * @author Luis Berrospi
+ */
+public class EJERCICIO_NUMERO_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
